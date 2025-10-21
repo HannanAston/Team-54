0b542b2e077ec01186600733a40d06523c601f99
RavingHausen <240166601@aston.ac.uk>
2025-10-21 14:39:56 +0100

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,3 +1,24 @@
 class Math {
-    
+    public int max(int a, int b){
+    if(a > b){
+        return a;
+    }
+    else if (b > a){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+public int min(int a, int b){
+    if(a > b){
+        return b;
+    }
+    else if (b > a){
+        return a;
+    }
+    else{
+        return a;
+    }
+}
 }
