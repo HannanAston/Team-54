2617923f93837dd7ade91ea80616af9c3b65dac6
Ahmiada Aston <230417850@aston.ac.uk>
2025-12-05 09:55:02 +0000

    Primitive front-end
    Primitive frontend

--- OurDatabase/resources/views/landing.blade.php
+++ OurDatabase/resources/views/landing.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Revival Threads</title>
 </head>
-<body>
+<body class="text-gray-500">
 
 
     <div> <!-- content wrapper -->
@@ -13,7 +14,7 @@
             <nav>
                 <div>
                     <h1>
-                        <a href="welcome"> Revival Threads </a>
+                        <a href="welcome" class="font-bold text-xl uppercase p-4"> Revival Threads </a>
                     </h1>
                 </div>
                 <div>
@@ -34,26 +35,37 @@
             </nav>
         </div> <!-- nav bar end-->
 
-        <main>
+        <main class="px-16 py-6">
             <div>
                 <a href="/login"> Log In </a>
                 <a href="/register"> Sign Up </a>
             </div>
 
-        <header> <h2> Armory </h2> </header>
+            <header> <h2 class="font-bold mt-10 pb-5"> Armory </h2> </header>
+            <div> <!-- clothes cards -->
 
-        <div> <!-- clothes cards -->
+                <div>
+                    <img src="{{ asset('img/1000_F_1242303249_J4PlMejurnyNXVkSyCbgr6cFWeOMfRcs.jpg') }}" alt="Cargos">
+                    <div>
+                        <span> Cargos </span>
+                    </div>
+                </div>
 
-            <div>
-                <img src="" alt="">
             </div>
 
-        </div>
+            <h2 class="font-bold mt-10 pb-5"> Timeless Gear </h2>
+            <div>
+                <!-- More clothes cards {fill in with armory code after styling} -->
 
-        <h4> Timeless Gear </h4>
+                <div>
+                    <div>
+                        Load More
+                    </div>
+                </div>
+            </div>
+
         </main>
     </div>
-
 
 </body>
 </html>
