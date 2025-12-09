061a252fa91218044a09a655e896811ebd1961bf
Ahmiada <230417850@aston.ac.uk>
2025-12-09 15:15:53 +0000

    Update TestProductSeeder.php
    added images to hoodie and trousers

--- OurDatabase/database/seeders/TestProductSeeder.php
+++ OurDatabase/database/seeders/TestProductSeeder.php
@@ -25,7 +25,7 @@ class TestProductSeeder extends Seeder
             'price' => 39.99,
             'stock_qty' => 50,
             'stock_threshold' => 5,
-            'image_url' => 'hoodie.jpg',
+            'image_url' => 'https://ruandrocka.co.uk/cdn/shop/files/GREYZIPHOODIEBACK.jpg?v=1723725056&width=1946',
             'category_id' => 11,
         ],
 
@@ -37,7 +37,7 @@ class TestProductSeeder extends Seeder
             'price' => 69.99,
             'stock_qty' => 40,
             'stock_threshold' => 5,
-            'image_url' => 'trouser.jpg',
+            'image_url' => 'https://cdn.media.amplience.net/i/frasersdev/57909103_o_a11?fmt=auto&upscale=true&w=450&h=450&sm=scaleFit&$h-ttl$',
             'category_id' => 12,
 
         ],
